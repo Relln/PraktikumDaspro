e8c66693049d25501897c7354391863aa7761102
Relln <farrel.novrizha@gmail.com>
2025-10-02 15:15:17 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKRS10.java
+++ jobsheet5/ifCetakKRS10.java
@@ -7,6 +7,7 @@ public class ifCetakKRS10 {
         
     Scanner sc = new Scanner(System.in);
 
+    //verifikasi pembayaran ukt
     System.out.println("--- Cetak KRS SIAKAD ---");
     System.out.print("Apakah UKT sudah lunas? (true/false): ");
     boolean uktLunas = sc.nextBoolean();
@@ -14,6 +15,12 @@ public class ifCetakKRS10 {
     if (uktLunas) {
         System.out.println("Pembayaran UKT terverifikasi");
         System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+    
+    }
+    else {
+        System.out.println("Registrasi ditolak");
+        System.out.println("Silahkan lunasi UKT terlebih dahulu");
+
     }
         }
             }
