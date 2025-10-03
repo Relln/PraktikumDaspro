be2caff2273aef0e3c41ea2c0085010770cfb7c5
Relln <farrel.novrizha@gmail.com>
2025-10-03 09:22:00 +0700

    Tugas 2

---
+++ jobsheet5/jobsheet5tugas2.java
@@ -0,0 +1,19 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class jobsheet5tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan angka :");
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Angka adalah genap");
+        } else {
+          System.out.println("Angka adalah ganjil");
+        }
+
+    }
+
+}
