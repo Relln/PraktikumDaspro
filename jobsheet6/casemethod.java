ba35f4fdb13efabc2bb93fbf81b5dab94319e2f1
Relln <farrel.novrizha@gmail.com>
2025-10-08 20:30:54 +0700

    casemethod

---
+++ jobsheet6/casemethod.java
@@ -0,0 +1,30 @@
+package jobsheet6;
+
+import java.util.Scanner;
+
+public class casemethod {
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+    String Nama;
+
+    System.out.print("==== INPUT DATA MAHASISWA ====");
+    System.out.print("Nama :")
+    String Nama = sc.nextLIne();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
