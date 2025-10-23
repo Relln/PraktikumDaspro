d7eae1badb19b357d3f593e0790159fb687a964a
Relln <farrel.novrizha@gmail.com>
2025-10-23 13:41:52 +0700

    Percobaan 1

---
+++ jobsheet7/SiakadFor10.java
@@ -0,0 +1,28 @@
+package jobsheet7;
+
+import java.util.Scanner;
+
+public class SiakadFor10{
+    
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+double nilai, tertinggi = 0, terendah = 100;
+
+for (int i = 1; i <= 10; i++) {
+    System.out.print("Masukan Nilai mahasiswa ke-" + i + ": ");
+    nilai = sc.nextDouble();
+    if ( nilai > tertinggi ) {
+        tertinggi = nilai;
+    }
+    if ( nilai < terendah ) {
+        terendah = nilai;
+    }
+}
+
+System.out.println("Nilai Tertinggi :" + tertinggi);
+System.out.println("Nilai Terendah : " + terendah);
+
+
+    }
+}
