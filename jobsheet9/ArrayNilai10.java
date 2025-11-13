da9df3dde83ad46f7265d0b409bfd90a46f95aa0
Relln <farrel.novrizha@gmail.com>
2025-11-13 11:18:06 +0700

    Modifikasi Percobaan 2

--- jobsheet9/ArrayNilai10.java
+++ jobsheet9/ArrayNilai10.java
@@ -6,14 +6,18 @@ public class ArrayNilai10 {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    int [] nilaiAkhir = new int[10];
+    int[] nilaiAkhir = new int[10];
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < nilaiAkhir.length; i++) {
         System.out.print("Masukkan Nilai Akhir ke-"+i+" : ");
             nilaiAkhir[i] = sc.nextInt(); 
     }
-    for (int i = 0; i < 10; i++) {    
-            System.out.println("Nilai Akhir ke-"+ i +" adalah "+nilaiAkhir[1]); 
+    for (int i = 0; i < nilaiAkhir.length; i++) {   
+        if (nilaiAkhir[i] > 70) {
+            System.out.println("Mahasiswa ke-"+i+" lulus!");
+        } else if (nilaiAkhir[i] < 70) {
+            System.out.println("Mahasiswa ke-"+i+" tidak lulus!");
+        }
     }
 
    
