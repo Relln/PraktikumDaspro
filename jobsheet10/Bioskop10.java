8f4297fc3459657671b42f0906832554a088d77a
Relln <farrel.novrizha@gmail.com>
2025-11-18 12:11:28 +0700

    Modifikasi Percobaan 1

--- jobsheet10/Bioskop10.java
+++ jobsheet10/Bioskop10.java
@@ -1,6 +1,6 @@
 package jobsheet10;
 
-public class bioskop10 {
+public class Bioskop10 {
     public static void main(String[] args) {
         
     String[][] penonton = new String[4][2];
@@ -14,16 +14,15 @@ public class bioskop10 {
     penonton[3][0] = "Gisel";
     penonton[3][1] = "Hana";
 
-    //System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
-    //System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
-    //System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
-    //System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
+    System.out.printf("%s \t %s\n", penonton[0][0], penonton[0][1]);
+    System.out.printf("%s \t %s\n", penonton[1][0], penonton[1][1]);
+    System.out.printf("%s \t %s\n", penonton[2][0], penonton[2][1]);
+    System.out.printf("%s \t %s\n", penonton[3][0], penonton[3][1]);
     
-    System.out.println(penonton.length);
-    System.out.println(penonton[0].length);
-    System.out.println(penonton[1].length);
-    System.out.println(penonton[2].length);
-    System.out.println(penonton[3].length);    
+    
+    for (int i = 0; i < penonton.length; i++) {
+        System.out.println("Penonton pada baris ke- " + (i+1) + ": " + String.join(", ", penonton[i]));
+    }
 
     }
 }
